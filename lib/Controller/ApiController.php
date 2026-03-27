a47b2be160bc70f7a9087bf17a9e750a4a79ad97
Fabien Cheftel <f.cheftel@gmail.com>
2026-03-27 10:26:27 +0100

    Several fixes - Compatible with Nextcloud32 - uses only public APIs - More performant - Future-proof

--- lib/Controller/ApiController.php
+++ lib/Controller/ApiController.php
@@ -5,6 +5,7 @@ use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\Files\IRootFolder;
 use OCP\IRequest;
+use OCP\Files\Node;
 use OC\Files\Filesystem;
 
 class ApiController extends Controller {
@@ -12,7 +13,12 @@ class ApiController extends Controller {
 	private IRootFolder $rootFolder;
 	private ?string $userId;
 
-	public function  __construct($appName, IRequest $request, IRootFolder $rootFolder, ?string $userId = null) {
+	public function __construct(
+		string $appName,
+		IRequest $request,
+		IRootFolder $rootFolder,
+		?string $userId = null
+	) {
 		parent::__construct($appName, $request);
 		$this->rootFolder = $rootFolder;
 		$this->userId = $userId;
@@ -21,34 +27,56 @@ class ApiController extends Controller {
 	/**
 	 * @NoAdminRequired
 	 */
-	public function files() {
-		$files = $this->getFilesRecursive();
-		$results = \OCA\Files\Helper::formatFileInfos($files);
-		foreach ($results as $key => $result) {
-			$path = $this->getRelativePath($files[$key]->getPath());
-			$results[$key]['path'] = $path;
-		}
+	public function files(): DataResponse {
+		$userFolder = $this->rootFolder->getUserFolder($this->userId);
+		$files = $this->getFilesRecursive($userFolder);
+
+		$results = array_map(function (Node $file) {
+			return [
+				'id' => $file->getId(),
+				'name' => $file->getName(),
+				'size' => $file->getSize(),
+				'mimetype' => $file->getMimeType(),
+				'mtime' => $file->getMTime(),
+				'path' => $this->getRelativePath($file->getPath()),
+			];
+		}, $files);
 
 		return new DataResponse($results);
 	}
 
-	private function getFilesRecursive(& $results = [], $path = '') {
-		$userFolder = $this->rootFolder->getUserFolder($this->userId);
-		$files = $userFolder->get($path)->getDirectoryListing();
-		foreach($files as $file) {
-			if ($file->getType() === 'dir') {
-				$this->getFilesRecursive($results, $path . '/' . $file->getName());
+	/**
+	 * Recursively collect all files
+	 */
+	private function getFilesRecursive(Node $folder, array &$results = []): array {
+		foreach ($folder->getDirectoryListing() as $node) {
+			if ($node->getType() === Node::TYPE_FOLDER) {
+				$this->getFilesRecursive($node, $results);
 			} else {
-				$results[] = $file->getFileInfo();
+				$results[] = $node;
 			}
 		}
 
 		return $results;
 	}
 
-	private function getRelativePath($path) {
-		$path = Filesystem::getView()->getRelativePath($path);
-		return substr($path, 0, strlen($path) - strlen(basename($path)));
-	}
+	/**
+	 * Convert absolute path to user-relative directory path
+	 */
+	private function getRelativePath(string $path): string {
+		$relative = Filesystem::getView()->getRelativePath($path);
 
+		if ($relative === null) {
+			return '';
+		}
+
+		// Define a maximum path length
+		$maxPathLength = 100; // Adjust this value as needed
+
+		if (strlen($relative) > $maxPathLength) {
+			$relative = substr($relative, -($maxPathLength - 3)) . '...'; // Truncate and add ellipsis
+		}
+
+		return dirname($relative) . '/';
+	}
 }

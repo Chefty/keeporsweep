e7e04a85b68829837f14a4cce6efcce3d100a313
Fabien Cheftel <f.cheftel@gmail.com>
2026-03-27 12:56:15 +0100

    Remove /appinfo/app.php not supported

--- appinfo/app.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use OCA\KeepOrSweep\AppInfo\Application;
-
-return new Application();
--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -4,17 +4,12 @@ declare(strict_types=1);
 
 namespace OCA\KeepOrSweep\AppInfo;
 
-use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
 
-class Application extends App implements IBootstrap {
+class Application implements IBootstrap {
 	public const APP_ID = 'keeporsweep';
-
-	public function __construct() {
-		parent::__construct(self::APP_ID);
-	}
 
 	public function register(IRegistrationContext $context): void {
 		// Intentionally empty for now: constructor autowiring is sufficient.

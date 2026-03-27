d51b928fa327c2044767f6604d6783d658109dfe
Fabien Cheftel <f.cheftel@gmail.com>
2026-03-27 13:04:56 +0100

    Following up previous fix

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace OCA\KeepOrSweep\AppInfo;
 
+use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
 
-class Application implements IBootstrap {
+class Application extends App implements IBootstrap {
 	public const APP_ID = 'keeporsweep';
+
+	public function __construct() {
+		parent::__construct(self::APP_ID);
+	}
 
 	public function register(IRegistrationContext $context): void {
 		// Intentionally empty for now: constructor autowiring is sufficient.
